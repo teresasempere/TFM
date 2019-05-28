2fb36473ee88b56899fbb983bef9cffffdcd9d42
teresasempere <teresa3tc@gmail.com>
2019-05-28 16:56:23 +0200

    Places and supports

--- main.js
+++ main.js
@@ -5,3 +5,49 @@
     var element = document.getElementById("menu");
     element.classList.toggle("show");
   }
+
+
+  /* --- Sticky header" --- 
+
+window.onscroll = function() {myFunction()};
+
+
+var navbar = document.getElementById("header");
+
+
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+} */
+
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
